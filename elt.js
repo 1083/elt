6dadefda32b091b3baa9bedf43f4407e3e0131de
Max Terry <mhterry@gmail.com>
2015-01-25 01:22:17 -0500

    Switch to spaces

--- elt.js
+++ elt.js
@@ -1,33 +1,33 @@
 function elt(it) {
-	if (typeof it == 'string') {
-		it = document.createElement(it)
-	}
-	return {
-		set: function(key, val) {
-			if (val) {
-				it.setAttribute(key, val)
-			}
-		},
-		push: function(x) {
-			if (x.self && x.self().blur) {
-				return it.appendChild(x.self())
-			}
-			else {
-				it.innerHTML += x
-			}
-		},
-		self: function() {
-			return it
-		},
-		get: function() {
-			return it.innerHTML
-		}
-	}
+  if (typeof it == 'string') {
+    it = document.createElement(it)
+  }
+  return {
+    set: function(key, val) {
+      if (val) {
+        it.setAttribute(key, val)
+      }
+    },
+    push: function(x) {
+      if (x.self && x.self().blur) {
+        return it.appendChild(x.self())
+      }
+      else {
+        it.innerHTML += x
+      }
+    },
+    self: function() {
+      return it
+    },
+    get: function() {
+      return it.innerHTML
+    }
+  }
 }
 
 elt.get = function(selector) {
-	var f = selector[0] == '#'? 'getElementById' :
-			selector[0] == '.'? 'getElementsByClassName' :
-			'getElementsByTagName'
-	return document[f](selector)
+  var f = selector[0] == '#'? 'getElementById' :
+          selector[0] == '.'? 'getElementsByClassName' :
+          'getElementsByTagName'
+  return document[f](selector)
 }
